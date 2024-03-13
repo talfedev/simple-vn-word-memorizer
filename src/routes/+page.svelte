5c8cb05612cd991184851b63edc904f8b2905979
talfedev <talfedev@gmail.com>
2024-03-13 13:16:05 +0700

    ready to start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    //
+</script>
+
+<div class="container">
+    <h1>Hello language learners!</h1>
+</div>
+
+<style lang="scss">
+    .container {
+        max-width: 800px;
+        margin: 0 auto;
+        text-align: center;
+        padding-top: 3rem;
+        
+        h1 {
+            color: rgb(49, 201, 80);
+            padding: 5px;
+            border-radius: 15px;
+        }
+    }
+
+</style>
