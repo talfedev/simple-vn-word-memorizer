7047c0ceb938ded30473f46a9c65f17bc1ebb335
talfedev <talfedev@gmail.com>
2024-03-19 14:23:49 +0700

    feat: completed basic edit words functionality

--- src/routes/edit/+page.svelte
+++ src/routes/edit/+page.svelte
@@ -2,34 +2,65 @@
 	import type { Word } from '$lib/constructors';
 	import { words } from '$lib/stores';
 
-    let wordSearch = '';
-    
-    $: wordList = [...$words].filter((word) => {
-        return word.en.includes(wordSearch) || word.vn.includes(wordSearch);
-    });
+	let wordSearch = '';
+
+	let dialogRef: HTMLDialogElement | null = null;
+
+	$: wordList = [...$words].filter((word) => {
+		return word.en.includes(wordSearch) || word.vn.includes(wordSearch);
+	});
+
+	let selectedWord = {
+		en: '',
+		vn: '',
+		index: -1
+	}
+
+	const editWord = (word: {index: number, en: string, vn: string}) => {
+		selectedWord.en = word.en;
+		selectedWord.vn = word.vn;
+		selectedWord.index = word.index;
+		dialogRef?.showModal();
+	};
+
+	const closeModal = (edit?: boolean) => {
+		if(edit) {
+			wordList[selectedWord.index].en = selectedWord.en;
+			wordList[selectedWord.index].vn = selectedWord.vn;
+		}
+		
+		// cleanup
+		selectedWord.en = '';
+		selectedWord.vn = '';
+		dialogRef?.close();
+	};
 </script>
 
 <main>
 	<h1>edit words</h1>
 	<br />
 	<br />
-    <input bind:value={wordSearch} placeholder="search word" type="text">
-    <br>
-    <br>
+	<input bind:value={wordSearch} placeholder="search word" type="text" />
+	<br />
+	<br />
 	<table>
 		<thead>
 			<tr>
-				<th>#</th>
+				<th class="number-td">#</th>
 				<th>English</th>
 				<th>Vietnamese</th>
+				<th class="level-td">Lv.</th>
+				<th class="actions-td"></th>
 			</tr>
 		</thead>
 		<tbody>
 			{#each wordList as word, index (word.en)}
 				<tr>
-					<td>{index}</td>
+					<td class="number-td">{index + 1}</td>
 					<td>{word.en}</td>
 					<td>{word.vn}</td>
+					<td class="level-td">{word.lvl}</td>
+					<td class="actions-td"><button on:click={() => editWord({index, ...word})}>Edit</button></td>
 				</tr>
 			{/each}
 		</tbody>
@@ -38,6 +69,22 @@
 	<br />
 	<a href="/">back</a>
 </main>
+
+<dialog bind:this={dialogRef}>
+	<div>
+		<h3>Edit word</h3>
+		<label for="">
+			English:
+			<input bind:value={selectedWord.en} type="text">
+		</label>
+		<label for="">
+			Vietnamese:
+			<input bind:value={selectedWord.vn} type="text">
+		</label>
+		<button on:click={() => closeModal(true)}>Edit</button>
+		<button on:click={() => closeModal()}>close</button>
+	</div>
+</dialog>
 
 <style lang="scss">
 	main {
@@ -48,21 +95,53 @@
 		padding-top: 2rem;
 	}
 
-    table, tr, td, th {
-        border: 1px solid black;
-    }
+	table,
+	tr,
+	td,
+	th {
+		border: 1px solid black;
+	}
 
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+	table {
+		border-collapse: collapse;
+		width: 100%;
+		table-layout: fixed;
+	}
 
 	tr {
 		background-color: #b9d1b9;
 	}
 
-	td, th {
+	td,
+	th {
 		text-align: left;
-        padding: 5px 10px;
+		padding: 5px 10px;
+		font-size: 14px;
+	}
+
+	.number-td {
+		width: 50px;
+	}
+
+	.actions-td {
+		width: 60px;
+
+		button {
+			padding: 5px;
+			border-radius: 4px;
+			border: 1px solid black;
+		}
+	}
+
+	.level-td {
+		width: 40px;
+	}
+
+	dialog {
+		margin: auto;
+
+		&::backdrop {
+			background-color: rgba(0, 0, 0, 0.5);
+		}
 	}
 </style>
